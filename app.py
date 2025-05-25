3a1e00048c6c5a201d687d340859412f461616bf
05rakshit <05rakshitgarg@gmail.com>
2025-05-25 23:59:47 +0530

    changes

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def check_number():
         return jsonify({'number_plate':number, "message":"no onwer found in db"})
     
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=10000)
+    port = int(os.environ.get("PORT", 5000))  # use Render's assigned port or default 5000
+    app.run(host="0.0.0.0", port=port)
