ca6459e4acd82aca26a2092c649a48d55a60f9b7
05rakshit <05rakshitgarg@gmail.com>
2025-06-01 00:26:33 +0530

    Changes

--- utils.py
+++ utils.py
@@ -1,33 +1,8 @@
 import cv2
 import numpy as np
 import imutils
-import easyocr
-import mysql.connector
-from mysql.connector import errorcode,connection
 
-
-# Function to connect to the MySQL database
-def connect_to_database():
-    try:
-        cnx = connection.MySQLConnection(
-            user='root',
-            password='QHdpjQYeAqWdLwnuIQPcwPwrHBcCwJFY',
-            host='mainline.proxy.rlwy.net',
-            database='railway',
-            port='55270'
-        )
-        return cnx
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your username or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-        return None
-
-#image processing and number extraction
-def extract_number_plate(img_path):
+def extract_number_plate(img_path, reader):
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -46,6 +21,9 @@ def extract_number_plate(img_path):
             location = approx
             break
 
+    if location is None:
+        return None
+
     mask = np.zeros(gray.shape, np.uint8)
     new_image = cv2.drawContours(mask, [location], 0, 255, -1)
     new_image = cv2.bitwise_and(img, img, mask=mask)
@@ -53,29 +31,13 @@ def extract_number_plate(img_path):
     (x, y) = np.where(mask == 255)
     (x1, y1) = (np.min(x), np.min(y))
     (x2, y2) = (np.max(x), np.max(y))
-    cropped_image = gray[x1:x2 + 1, y1:y2]
+    cropped_image = gray[x1:x2 + 1, y1:y2 + 1]
 
-    reader = easyocr.Reader(['en'])
     result = reader.readtext(cropped_image)
 
     if result:
         text = result[0][-2]
-        if text == "IND":
+        if text == "IND" and len(result) > 1:
             text = result[1][-2]
-        return text.replace(" ","")
-    return None
-
-# Look up in MySQL
-def get_owner_details(number_plate):
-    cnx = connect_to_database()
-    if cnx:
-        cursor = cnx.cursor()
-        
-        cursor.execute("SELECT owner_name, phone_number, address FROM vehicle_owner WHERE number_plate = %s", (number_plate,))
-        owner_info = cursor.fetchone()
-
-        cursor.close()
-        cnx.close()
-
-        return owner_info
+        return text.replace(" ", "")
     return None
