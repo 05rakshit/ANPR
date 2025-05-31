31bed2a59cc2ed634f24173ddf515058f32a1ee3
05rakshit <05rakshitgarg@gmail.com>
2025-06-01 01:01:00 +0530

    Changes

--- utils.py
+++ utils.py
@@ -1,7 +1,31 @@
 import cv2
 import numpy as np
 import imutils
+import mysql.connector
+from mysql.connector import errorcode,connection
 
+
+# Function to connect to the MySQL database
+def connect_to_database():
+    try:
+        cnx = connection.MySQLConnection(
+            user='root',
+            password='QHdpjQYeAqWdLwnuIQPcwPwrHBcCwJFY',
+            host='mainline.proxy.rlwy.net',
+            database='railway',
+            port='55270'
+        )
+        return cnx
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your username or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        return None
+
+#image processing and number extraction
 def extract_number_plate(img_path, reader):
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -40,4 +64,19 @@ def extract_number_plate(img_path, reader):
         if text == "IND" and len(result) > 1:
             text = result[1][-2]
         return text.replace(" ", "")
+    return None
+
+# Look up in MySQL
+def get_owner_details(number_plate):
+    cnx = connect_to_database()
+    if cnx:
+        cursor = cnx.cursor()
+        
+        cursor.execute("SELECT owner_name, phone_number, address FROM vehicle_owner WHERE number_plate = %s", (number_plate,))
+        owner_info = cursor.fetchone()
+
+        cursor.close()
+        cnx.close()
+
+        return owner_info
     return None
